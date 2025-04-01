1b73bfac18a4b61d64df834f350adfd5900ba924
donjerick <jerick.coneras@gmail.com>
2025-04-01 23:56:32 +0800

    chore: update project configurations and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  compiler: {
+    // Remove console logs in production
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
 };
 
 export default nextConfig;
